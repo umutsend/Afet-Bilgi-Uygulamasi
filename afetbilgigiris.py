6fc5f413afe421983345a477c3b35fcf770d0ade
umutsend <umut53701@gmail.com>
2023-06-09 19:30:34 +0300

    a

--- afetbilgigiris.py
+++ afetbilgigiris.py
@@ -1,86 +1,4 @@
-iller={
-"adana":["--- ADANA ---\nDeprem riski =  2. Derece\n1900-2013 arası sel felaketi sayısı = 1-4\n1900-2013 Çığ felaketi sayısı = 1-5 \n1900-2013 arası heyelan sayı aralığı = 1-10 "],
-"adıyaman":["--- ADIYAMAN ---\nDeprem riski = 1. Derece\n1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı  =  5-10 \n1900-2013 arası heyelan sayı aralığı = 20-30 "],
-"afyonkarahisar":["---AFYONKARAHİSAR ---\nDeprem riski = 2. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı = 1-10 "],
-"ağrı":["--- AĞRI ---\nDeprem riski = 2. Derece\n 1900-2013 arası sel felaketi sayısı = 10-20 \n 1900-2013 Çığ felaketi sayısı = 5-10 \n1900-2013 arası heyelan sayı aralığı =  10-20"],
-"amasya=":["--- AMASYA ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 10-20 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  20-30"],
-"ankara":["--- ANKARA ---\nDeprem riski = 3. Derece\n 1900-2013 arası sel felaketi sayısı = 5-10 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  10-20"],
-"antalya":["--- ANTALYA ---\nDeprem riski = 2. Derece\n 1900-2013 arası sel felaketi sayısı = 1-5 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"artvin":["--- ARTVİN ---\nDeprem riski = 4. Derece\n 1900-2013 arası sel felaketi sayısı = 1-5 \n 1900-2013 Çığ felaketi sayısı = 20-25 \n1900-2013 arası heyelan sayı aralığı =  50"],
-"aydın":["--- AYDIN ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 1-5 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"balıkesir":["--- BALIKESİR ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"bilecik":["--- BALIKESİR ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"bingöl":["--- BİNGÖL ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 10-20 \n 1900-2013 Çığ felaketi sayısı = 25-30 \n1900-2013 arası heyelan sayı aralığı =  40-50"],
-"bitlis":["--- BİTLİS ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 10-20 \n 1900-2013 Çığ felaketi sayısı = 30 \n1900-2013 arası heyelan sayı aralığı =  20-30"],
-"bolu":["--- BOLU ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"burdur":["--- BURDUR ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  0"],
-"bursa":["--- BURSA ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 0 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  10-20"],
-"çanakkale":["--- ÇANAKKALE ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"çankırı":["--- ÇANAKKALE ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"çorum":["--- ÇORUM ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı =  0\n1900-2013 arası heyelan sayı aralığı =  10-20"],
-"denizli":["--- DENİZLİ ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"diyarbakır":["--- DİYARBAKIR ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 0 \n 1900-2013 Çığ felaketi sayısı = 1-5 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"edirne":["--- EDİRNE ---\nDeprem riski = 4. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"elazığ":["--- ELAZIĞ ---\nDeprem riski = 2. Derece\n 1900-2013 arası sel felaketi sayısı = 0 \n 1900-2013 Çığ felaketi sayısı = 10-15 \n1900-2013 arası heyelan sayı aralığı =  10-20"],
-"erzincan":["--- ERZİNCAN ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 5-9 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  30-40"],
-"erzurum":["--- ERZURUM ---\nDeprem riski = 2. Derece\n 1900-2013 arası sel felaketi sayısı = 30-40 \n 1900-2013 Çığ felaketi sayısı = 15-20 \n1900-2013 arası heyelan sayı aralığı =  50"],
-"eskişehir":["--- ESKİŞEHİR ---\nDeprem riski = 2. Derece\n 1900-2013 arası sel felaketi sayısı = 0 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı = 0 "],
-"gaziantep":["--- GAZİANTEP ---\nDeprem riski = 4. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı = 0 "],
-"giresun":["--- GİRESUN ---\nDeprem riski = 3. Derece\n 1900-2013 arası sel felaketi sayısı = 0 \n 1900-2013 Çığ felaketi sayısı = 1-5 \n1900-2013 arası heyelan sayı aralığı =  40-50"],
-"gümüşhane":["--- GÜMÜŞHANE ---\nDeprem riski = 3. Derece\n 1900-2013 arası sel felaketi sayısı = 5-9 \n 1900-2013 Çığ felaketi sayısı = 1-5 \n1900-2013 arası heyelan sayı aralığı = 20-30 "],
-"hakkari":["--- HAKARİ ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 30-40 \n 1900-2013 Çığ felaketi sayısı = 20-25 \n1900-2013 arası heyelan sayı aralığı = 1-10 "],
-"hatay":["--- HATAY ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 0 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"ısparta":["--- ISPARTA ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"mersin":["--- MERSİN ---\nDeprem riski = 4. Derece\n 1900-2013 arası sel felaketi sayısı = 10-19 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"istanbul":["--- İSTANBUL ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"izmir":["--- İZMİR ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"kars":["--- KARS ---\nDeprem riski = 2. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 1-5 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"kastamonu":["--- KASTAMONU ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı =0  \n 1900-2013 Çığ felaketi sayısı = 1-5 \n1900-2013 arası heyelan sayı aralığı =  50"],
-"kayseri":["--- KAYSERİ ---\nDeprem riski = 3. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"kırklareli":["--- KIRKLARELİ ---\nDeprem riski = 4. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  0"],
-"kırşehir":["--- KIRŞEHİR ---\nDeprem riski =1 . Derece\n 1900-2013 arası sel felaketi sayısı = 0 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"kocaeli":["--- KOCAELİ ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"konya":["--- KONYA ---\nDeprem riski = 4. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 1-5 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"kütahya":["--- KÜTAHYA ---\nDeprem riski = 3. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  0"],
-"malatya":["--- MALATYA ---\nDeprem riski = 2. Derece\n 1900-2013 arası sel felaketi sayısı = 0 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  20-30"],
-"manisa":["--- MANİSA ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 0 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  10-20"],
-"kahramanmaraş":["--- KAHRAMANMARAŞ ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 0 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  30-40"],
-"mardin":["--- MARDİN ---\nDeprem riski = 4. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  0"],
-"muğla":["--- MUĞLA ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı =  0\n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"muş":["--- MUŞ ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  10-20"],
-"nevşehir":["--- NEVŞEHİR ---\nDeprem riski = 3. Derece\n 1900-2013 arası sel felaketi sayısı = 0 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  0"],
-"niğde":["--- NİĞDE ---\nDeprem riski = 4. Derece\n 1900-2013 arası sel felaketi sayısı = 0 \n 1900-2013 Çığ felaketi sayısı = 1-5 \n1900-2013 arası heyelan sayı aralığı =  0"],
-"ordu":["--- ORDU ---\nDeprem riski = 3. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 1-5 \n1900-2013 arası heyelan sayı aralığı =  20-30"],
-"rize":["--- RİZE ---\nDeprem riski = 4. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 1-5 \n1900-2013 arası heyelan sayı aralığı =  50"],
-"sakarya":["--- SAKARYA ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı =  0\n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"samsun":["--- SAMSUN ---\nDeprem riski = 3. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  20-30"],
-"siirt":["--- SİİRT---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 1-5 \n1900-2013 arası heyelan sayı aralığı =  10-20"],
-"sinop":["--- SİNOP ---\nDeprem riski = 4. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  30-40"],
-"sivas":["--- SİVAS ---\nDeprem riski = 3. Derece\n 1900-2013 arası sel felaketi sayısı = 0 \n 1900-2013 Çığ felaketi sayısı = 1-5 \n1900-2013 arası heyelan sayı aralığı =  40-50"],
-"tekirdağ":["--- TEKİRDAĞ ---\nDeprem riski = 3. Derece\n 1900-2013 arası sel felaketi sayısı = 0 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"tokat":["--- TOKAT ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  10-20"],
-"trabzon":["--- TRABZON ---\nDeprem riski = 4. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı = 50 "],
-"tunceli":["--- TUNCELİ ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 20-25 \n1900-2013 arası heyelan sayı aralığı = 20-30"],
-"şanlıurfa":["--- ŞANLIURFA ---\nDeprem riski = 4. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"uşak":["--- UŞAK ---\nDeprem riski = 2. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  0"],
-"van":["--- VAN ---\nDeprem riski = 2. Derece\n 1900-2013 arası sel felaketi sayısı =  \n 1900-2013 Çığ felaketi sayısı = 15-20 \n1900-2013 arası heyelan sayı aralığı = 10-20 "],
-"yozgat":["--- YOZGAT ---\nDeprem riski = 3. Derece\n 1900-2013 arası sel felaketi sayısı =  0\n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı = 1-10 "],
-"zonguldak":["--- ZONGULDAK ---\nDeprem riski = 2. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4  \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı = 1-10 "],
-"aksaray":["--- AKSARAY ---\nDeprem riski = 5. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"bayburt":["--- BAYBURT ---\nDeprem riski = 3. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı =  0\n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"karaman":["--- KARAMAN ---\nDeprem riski = 5. Derece\n 1900-2013 arası sel felaketi sayısı = 0 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"kırıkkale":["--- KIRIKKALE ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı =  0\n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"batman":["--- BATMAN ---\nDeprem riski = 2. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 1-5 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"şırnak":["--- ŞIRNAK ---\nDeprem riski = 2. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 1-5 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"bartın":["--- BARTIN ---\nDeprem riski = 3. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  10-20"],
-"ardahan":["--- ARDAHAN ---\nDeprem riski = 2. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı =  0\n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"ığdır":["--- IĞDIR ---\n riski = 2. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"yalova":["--- YALOVA ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 0 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"karabük":["--- KARABÜK ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  10-20"],
-"kilis":["--- KİLİS ---\nDeprem riski = 4. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-"osmaniye":["--- OSMANİYE ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 1-4 \n 1900-2013 Çığ felaketi sayısı =  0\n1900-2013 arası heyelan sayı aralığı =  0"],
-"düzce":["--- DÜZCE ---\nDeprem riski = 1. Derece\n 1900-2013 arası sel felaketi sayısı = 0 \n 1900-2013 Çığ felaketi sayısı = 0 \n1900-2013 arası heyelan sayı aralığı =  1-10"],
-}
+
 
 def AfetBilgileri(k_adi):
     AfetBilgi=input("Afetler hakkında bilgi almaya hoş geldin {}. Bilgi almak istediğin afeti gir.\n1-Deprem nedir?\n2-Sel nedir?\n3-Çığ nedir?\n4-Heyelan nedir?\n5-Deprem çantasında neler olmalı.\n".format(k_adi))
@@ -124,26 +42,25 @@ def DepremTesti():
         else:
             print("-- Hatalı giriş oldu tekrar girin --")
             continue
-def İlBilgileri(k_adi,iller):
-       while(True):  
-        il_secim=str(input("İl seçim ekranına hoş geldin {} bilgi almak istediğin ili yazman yeterli. çıkmak için çıkış yazabilirsin.\n".format(k_adi)))
-        if il_secim in iller:
-            print(iller[il_secim])
-        elif il_secim=="çıkış":
-            break
-        else:
-            print("Yanlış giriş yaptınız.")
-       
-
-    
-
-
 import sqlite3
 conn = sqlite3.connect('KullaniciHesaplari.db')
+
 c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS users
              (k_adi TEXT, sifre TEXT, Yasadigi_il TEXT, bagis REAL)''')
-
+def İlBilgileri():
+           connect = sqlite3.connect('afetbilgileri.db')
+           a = connect.cursor()  
+           girilen_sehir=input("Sorgulamak istediğiniz şehir adı nedir? : ")
+           a.execute('SELECT * FROM sehirler depremriski, digerbilgiler WHERE il = ?', (girilen_sehir,))
+           result = a.fetchone()
+           if result:
+               deprem_riski = result[0]
+               diger_bilgiler = result[1]
+               print(f"{il} şehri için deprem riski: {deprem_riski}")
+               print(f"{il} şehri için diğer bilgiler: {diger_bilgiler}")
+           else:
+               print("Belirtilen şehir bulunamadı.")
 def deprembagisi():
     print("Deprem bağışı yapıyor olman bizi çok mutlu etti.")
     c.execute("SELECT SUM(bagis) FROM users")
@@ -152,7 +69,7 @@ def deprembagisi():
         toplam_bagis = 0.0
     print("An itibariyle toplam bağış miktarı:", toplam_bagis)
     bagis_miktari = float(input("Bağışlamak istediğiniz tutar : "))
-    c.execute("INSERT INTO users (k_adi, sifre, Yasadigi_il,bagis) VALUES (?,?,?,?)", (k_adi, sifre, il,bagis_miktari,))
+    c.execute("INSERT INTO users (k_adi, sifre, Yasadigi_il, bagis) VALUES (?,?,?,?)", (k_adi, sifre, il, bagis_miktari))
     conn.commit()
     print("Bağışınız başarıyla kayedildi.")
     
@@ -160,7 +77,7 @@ def deprembagisi():
 while(True):
     
     print("Afet bilgi programına hoş geldiniz.")
-    print("1-Giriş yap\n2-Kaydol\n3-Şifremi unuttum")
+    print("1-Giriş yap\n2-Kaydol\n")
     secim1=input()
     if secim1=="1":
         k_adi=input("Kullanıcı adınız : ")
@@ -192,7 +109,7 @@ while(True):
     print("5-Çıkış yap")
     secim2=int(input())
     if secim2==1:
-        İlBilgileri(k_adi, iller)
+        İlBilgileri()
     elif secim2==2:
         AfetBilgileri(k_adi)
     elif secim2==3:
